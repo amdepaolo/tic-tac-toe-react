b8a791de73b1917bd1907b14eb9035df30edc5e3
Adam DePaolo <amdepaolo@gmail.com>
2024-01-29 16:22:27 -0500

    game handles cell clicks

--- src/assets/components/Game.jsx
+++ src/assets/components/Game.jsx
@@ -13,27 +13,43 @@ function Game(){
         c2: " ",
         c3: " "
     })
+    const [currentPlayer, setPlayer] = useState("X")
 
+    function swapPlayer(){
+        if (currentPlayer === "X"){
+            setPlayer("O")
+        } else setPlayer("X")
+    }
 
+    function claimCell(cell){
+        const newGameState = {...gameState, [cell]: currentPlayer}
+        setGameState(newGameState)
+    }
+
+    function gameMove(cell){
+        claimCell(cell)
+        swapPlayer()
+    }
 
     return(
         <div>
             <h1> This will be a tic tac toe game... soon </h1>
+            <h3>Current Player: {currentPlayer}</h3>
             <table>
                 <tr>
-                    <td>{gameState.a1}</td>
-                    <td>{gameState.a2}</td>
-                    <td>{gameState.a3}</td>
+                    <td onClick={()=>gameMove("a1")}>{gameState.a1}</td>
+                    <td onClick={()=>gameMove("a2")}>{gameState.a2}</td>
+                    <td onClick={()=>gameMove("a3")}>{gameState.a3}</td>
                 </tr>
                 <tr>
-                    <td>{gameState.b1}</td>
-                    <td>{gameState.b2}</td>
-                    <td>{gameState.b3}</td>
+                    <td onClick={()=>gameMove("b1")}>{gameState.b1}</td>
+                    <td onClick={()=>gameMove("b2")}>{gameState.b2}</td>
+                    <td onClick={()=>gameMove("b3")}>{gameState.b3}</td>
                 </tr>
                 <tr>
-                    <td>{gameState.c1}</td>
-                    <td>{gameState.c2}</td>
-                    <td>{gameState.c3}</td>
+                    <td onClick={()=>gameMove("c1")}>{gameState.c1}</td>
+                    <td onClick={()=>gameMove("c2")}>{gameState.c2}</td>
+                    <td onClick={()=>gameMove("c3")}>{gameState.c3}</td>
                 </tr>
             </table>
         </div>
